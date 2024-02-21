d1101ee33fe89a55c529fcda4472b7d64551b76a
Karthik M S <k2g1001@gmail.com>
2024-02-21 20:53:59 +0530

    update:company name

--- src/components/NameBio.jsx
+++ src/components/NameBio.jsx
@@ -1,11 +1,16 @@
 const NameBio = () => {
-    return (
-      <>
-        {" "}
-        <h1 className="name">Karthik M S</h1>
-        <h2 className="bio">Freelance React Developer</h2>
-      </>
-    );
-  };
+  return (
+    <>
+      {" "}
+      <h1 className="name">Karthik M S</h1>
+      <h2 className="bio">
+        Software Developer @{" "}
+        <a href="https://epoqzero.com/#/" className="company" target="_blank">
+          Epoq Zero LLP
+        </a>{" "}
+      </h2>
+    </>
+  )
+}
 
-  export default NameBio
+export default NameBio
