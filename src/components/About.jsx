01c87198ea8fed3b5b9ebd9216bde5f88777a0ab
Karthik M S <k2g1001@gmail.com>
2024-03-09 09:20:58 +0530

    about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,7 +4,20 @@ const About = () => {
       id="about"
       className="h-screen w-full flex flex-col items-center gap-4 justify-center snap-center"
     >
-      About
+      <div className="max-w-[700px] w-full flex flex-col items-left justify-center gap-4 px-6">
+        <p className="text-4xl sm:text-6xl font-bold">Karthik M S</p>
+        <p className="text-[#696560]">
+          Developer / Designer / Anime Enthusiast
+        </p>
+        <p className="font-medium text-[#575757]">
+          I’m a front-end developer based in India. I’m a self-taught developer
+          and my expertise is in developing web applications and websites using
+          React, Javascript and other front-end technologies. I love using my
+          creative and design skills to solve problems in the front-end.
+          Currently, I work as a Software Engineer at Epoq Zero LLP, creating
+          and implementing front-end applications for them and their clients.
+        </p>
+      </div>
     </div>
   )
 }
