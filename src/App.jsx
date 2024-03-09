b6d9023a39e4e7c3694468a04ae937a9cfbf5ae2
Karthik M S <k2g1001@gmail.com>
2024-03-09 11:00:06 +0530

    minor issue fix

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,9 @@ import Contact from "@/components/sections/Contact"
 
 const App = () => {
   return (
-    <div className="bg-backgroundColor text-textColor flex items-center flex-col relative ">
+    <div className="bg-backgroundColor text-textColor h-[100svh] flex items-center flex-col relative ">
       <Header />
-      <div className="h-[100svh] w-full  overflow-y-scroll snap-y snap-mandatory no-scrollbar">
+      <div className="h-screen w-full  overflow-y-scroll snap-y snap-mandatory no-scrollbar">
         <Main />
         <About />
         <Projects />
